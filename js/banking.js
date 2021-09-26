7da3dc5a17521db3449e5c81fcb9006a2bcb8890
Mohammad Foysal <mh.foysal.h@gmail.com>
2021-09-26 21:20:24 +0600

    update with input validation

--- js/banking.js
+++ js/banking.js
@@ -1,5 +1,6 @@
 //input field function
 function getInputValue(depositId) {
+    //debugger;
     const inputField = document.getElementById(depositId);
     const inputAmountText = inputField.value;
     const inputAmountValue = parseFloat(inputAmountText);
@@ -17,12 +18,19 @@ function updateTotalField(totalID, totalAmount) {
     total.innerText = totalAmount + previousTotal;
     return total;
 }
+//get current balance 
+function getCurrentBalance() {
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    return previousBalanceTotal;
+}
 
 //Total Balance update fuction
 function totalBalance(amount, isadd) {
     const balanceTotal = document.getElementById('balance-total');
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    const previousBalanceTotal = getCurrentBalance();
     //if isadd == true then add the balance else isadd == false minus the balance
     if (isadd == true) {
         balanceTotal.innerText = previousBalanceTotal + amount;
@@ -30,26 +38,43 @@ function totalBalance(amount, isadd) {
     else {
         balanceTotal.innerText = previousBalanceTotal - amount;
     }
-    return balanceTotal;
+    //return balanceTotal;
 }
 
 //-------------------handle deposit btn---------------------
 document.getElementById('deposit-btn').addEventListener('click', function () {
 
     //get new deposite balance 
-    const previousDepositAmount = getInputValue('deposit-input');
-    // get current deposite balance and update with new Balance
-    updateTotalField('deposite-total', previousDepositAmount);
-    // get previous Total balance and update new Balance
-    totalBalance(previousDepositAmount, true);
+    const depositAmount = getInputValue('deposit-input');
+    if (depositAmount > 0) {
+        // get current deposite balance and update with new Balance
+        updateTotalField('deposite-total', depositAmount);
+        // get previous Total balance and update new Balance
+        totalBalance(depositAmount, true);
+    }
+    else {
+        alert('Please input a positve number for deposit');
+    }
 });
 
 //-------------------handle withdraw btn---------------------
 document.getElementById('withdraw-btn').addEventListener('click', function () {
     //get new withdraw balance 
     const newWithdrawInput = getInputValue('withdraw-input');
-    //get current withdraw amount and add new withdraw balance
-    updateTotalField('withdraw-total', newWithdrawInput);
-    // get previous Total balance and update new Balance
-    totalBalance(newWithdrawInput, false);
+    const currentBalance = getCurrentBalance();
+    if (newWithdrawInput > 0) {
+        if (newWithdrawInput <= currentBalance) {
+            //get current withdraw amount and add new withdraw balance
+            updateTotalField('withdraw-total', newWithdrawInput);
+            // get previous Total balance and update new Balance
+            totalBalance(newWithdrawInput, false);
+        }
+        else {
+            alert('You can not withdraw more than what you have in your account');
+        }
+    }
+
+    else {
+        alert('Please input a positve number');
+    }
 });
